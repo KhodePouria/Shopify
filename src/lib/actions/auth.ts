740f4f852999ffd4e6a66b8b1906cbd2698d4e6a
Pouria <pooriaramezani1382@gmail.com>
2025-07-08 12:41:13 +0330

    add google authorization

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -2,10 +2,12 @@
 
 import { signIn,signOut } from "@/auth";
 
-export const login = async ()=>{
+export const logingit = async ()=>{
     await signIn("github", {redirectTo:"/"});
 };
-
+export const logingoogle = async ()=>{
+    await signIn("google", {redirectTo:"/"});
+};
 export const logout = async ()=>{
     await signOut({redirectTo:"/"});
 };
